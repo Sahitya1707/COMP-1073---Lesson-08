0c2ed4d3378ca41428646264120fe7cd36e52536
Sahitya hp <neupanesahitya1@gmail.com>
2024-03-06 10:57:30 -0500

    completed

--- js/objects.js
+++ js/objects.js
@@ -1,7 +1,21 @@
-const output = document.getElementById('output');
+const output = document.getElementById("output");
 
 /* STEP 1: Create an object (looks a lot like declaring a variable, but with empty braces), then open this page in a browser and enter 'person' in the console */
+const person = {
+  name: {
+    first: "Sahitya",
+    last: "Neupane",
+  },
+  age: 58,
+  interest: ["Camping", "Learning", "Playing"],
+  greeting() {
+    // alert(`Hi, I'm ${this.name} and I am ${this.age} years old.`);
+    let greeting = `Hi, I'm ${this.name.first} and I am ${this.age} years old and I enjoy ${this.interest[2]}.`;
 
+    return greeting;
+  },
+};
+// console.log(person.greeting());
 
 /* STEP 2a: Add other data items to the above object, like name, age, gender, and interests */
 
@@ -20,40 +34,56 @@ const output = document.getElementById('output');
 
 /* STEP 5a: Dot syntax allows you to access the parts of an object - the first part is the namespace, and subsequent parts are used to access things encapsulated within the object. Access the first name, last name, and age of the person object, and put it inside the <p id="output"></p>. */
 
-
 /* STEP 5b: Update the above greeting() function inside the person object to return the string, instead of the alert(). */
-
-
+output.textContent = `${person["name"]["first"]} ${person["name"]["last"]}`;
 /* STEP 6a: An object can even contain another object - update object person above to include a name object. */
-
+person.name.first = "Don";
+output.textContent = `${person["name"]["first"]} ${person["name"]["last"]}`;
 /* STEP 6b: Note how this breaks the greeting function - update the greeting function to use this sub-namespace */
 
-
 /* STEP 7: You can also use bracket notation, just like an array - objects that are built this way are often referred to as associative arrays */
-
 
 /* STEP 8a: You can also set members of an object - try changing the person's name, and age */
 
 /* STEP 8b: Bracket notation allows you to dynamically refer to property names. Create a function below that accepts a property name as an argument, then console.log() that property value. Try it out! */
 
+function logProperty(propname) {
+  console.log(person[propname]);
+}
 
+// console.log(logProperty(age));
 /* STEP 9a: It it also possible to add new members to an object. Type person.eyes = "YOUR_EYE_COLOUR_HERE" into the console - and then type person.eyes */
 
-
 /* STEP 9b: Add a new function called 'goodbye' to the person object and try it by typing person.goodbye into the console */
-
-
+function goodbye() {
+  let goodbye = `${this.name.first} says goodbye`;
+  return goodbye;
+}
 /* STEP 10: We can also dynamically set both names and values for objects (using bracket syntax)… */
-
+const dataName = "height";
+const dataValue = "1.7m";
+person[dataName] = dataValue;
+console.log(person);
 
 /* STEP 11a: 'this' is very useful - it allows us to refer to the object in question, specifically. This will become much clearer later in the course. Create two new objects, each with a name, and a function that outputs the name */
+const person1 = {
+  name: "don",
+  introduceSelf() {
+    console.log(`Hi. I'm ${this.name}`);
+  },
+};
 
-
+const person2 = {
+  name: "Suman",
+  introduceSelf() {
+    console.log(`Hi. I'm ${this.name}`);
+  },
+};
 /* STEP 11b: Call up both person1.hello() and person2.hello() in the console to see how 'this' is specific to each object */
 
 // You have already been using objects all along:
-let myString = 'Words, words, words.'; // this creates an instance of the class String
-const elem = document.getElementById('output'); // this accesses the Document object, which is created (instantiated) when the browser opens the HTML page
+let myString = "Words, words, words."; // this creates an instance of the class String
+const elem = document.getElementById("output"); // this accesses the Document object, which is created (instantiated) when the browser opens the HTML page
 
 // Next, open up oop.html and we will delve a bit deeper into JavaScript objects.
 
--- js/oop.js
+++ js/oop.js
@@ -1,24 +1,44 @@
-const output = document.getElementById('output');
+const output = document.getElementById("output");
 
 /* STEP 1a: Create a new object using a regular function */
-
+function createAlbum(name) {
+  const obj = {};
+  obj.name = name;
+  obj.describe = function () {
+    alert(`This album is called ${this.name}`);
+  };
+  return obj;
+}
 
 /* STEP 1b: Use the console to create a new vehicle object, and then invoke the function represented using .description() */
-
+// function Album(name) {
+//   this.name = name;
+//   this.describe = function () {
+//     alert(`This album is called ${this.name}`);
+//   };
+// }
 
 /* STEP 2a: In order to be a bit more concise, JavaScript allows us to use constructor functions - rewrite the above function, without returning anything. Capitalize the name of the function. */
 
-
 /* STEP 2b: Use the console to create a couple of different albums, using the 'new' keyword, and again invoking the .describe() method for each one */
 
-
 /* STEP 3a: Build the complete constructor for the object Vehicle (comment out the above functions first). Include full vehicle name (manufacturer and model), year, type, colour, and description (function). */
-
-
-
+function Album(name, artist, year, numTracks) {
+  this.name = name;
+  this.artist = artist;
+  this.year = year;
+  this.numTracks = numTracks;
+  this.describe = function () {
+    let description = `The album ${this.name} by ${this.artist} was released in ${this.year} featuring ${this.numTracks} song tracks.`;
+    return description;
+  };
+}
 /* STEP 3b: Instantiate a new Album (or 2) based on the above constructor */
+const album2 = new Album("Disintegration", "The cure", 1989, 12);
+const album3 = new Album("Nepatya", "The curse", 2009, 50);
 
-
+// album2.describe();
+// console.log(album2);
 /* STEP 3c: Attempt to access the various properties of album2 or album3 using the console. */
 // album2['name']
 // album2['artist']
@@ -31,8 +51,7 @@ const output = document.getElementById('output');
 /* STEP 4a: Modify the describe method of the above Album constructor so that it returns a string. */
 
 /* STEP 4b: Capture the description of one of the album objects and set it as the text for the paragraph on the page (see line 1 of this script). */
-
-
+output.textContent = album2.describe();
 // That's it! Now on to the Lab...
 
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Basics
